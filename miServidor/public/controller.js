c78c102d34a8da5a4bf1557b6e0241d84db72514
Angelica Guaman <angelica.guaman.albarracin@gmail.com>
2017-04-12 17:12:18 +0200

    se añade el servidor express utilizando nodejs: sudo ppm install -g express-generator > express miServidor > posicionarnos en nuestro servidor > npm install >npm start > copiar los fichero index, styles en la carpeta public

---
+++ miServidor/public/controller.js
@@ -0,0 +1,31 @@
+var app=angular.module("app",[]);
+  
+app.controller('AlumnosController',['$scope', '$log', function($scope, $log){
+
+	$scope.formVisibility=false;
+
+	$scope.alumnos = [ //modelo
+		{nombre: "Robinson Ganchala", telefono:"123456789", curso: "Cuarto"	},
+		{nombre: "Juan Carlos García", telefono:"987654321", curso: "Cuarto" },
+		{nombre: "Javier Bodas", telefono:"543216789", curso:"Primero" },
+		{nombre: "Angélica Guamán Albarracín", telefono: "678954321", curso:"Segundo"}
+	];
+
+	$scope.nuevoAlumno = {
+		nombre:"",
+		telefono:"",
+		curso:""
+	};
+
+	$scope.save = function()
+	{
+		$scope.alumnos.push($scope.nuevoAlumno);
+		$scope.nuevoAlumno={};
+		$scope.formVisibility=false;
+	};
+
+	$scope.showForm = function(){
+		$scope.formVisibility=true;
+		console.log($scope.formVisibility);
+	};
+}]); 
