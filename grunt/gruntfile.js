24677f5a8461fba0b282adf41d8ad3f8c2c4dc9d
Angelica Guaman <angelica.guaman.albarracin@gmail.com>
2017-04-24 16:41:50 +0200

    task concat and uglify with grunt

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -4,12 +4,28 @@ module.exports = function(grunt) {
   	grunt.initConfig({
   		jshint:{
   			all:['controller.js']
+  		},
+
+  		concat:{ //tarea 1: mezclar los archivos
+  			dist:{
+  				src:['controller.js', "controller1.js", "controller2.js"], //tiene que de misma secuencia en la que deben ser concatenados
+  				dest: 'unidos.js'
+  			}
+  		},
+
+  		uglify:{ //Tarea2: minimizar el archivo
+  			dist:{
+  				src:'unidos.js',
+  				dest:'build/unidos.min.js'
+  			}
   		}
   	});
 
   	// Load the plugin that provides the "uglify" task.
  	grunt.loadNpmTasks('grunt-contrib-jshint');
+ 	grunt.loadNpmTasks('grunt-contrib-concat');
+ 	grunt.loadNpmTasks('grunt-contrib-uglify');
 
   	// Default task(s).
-  	grunt.registerTask('default', ['jshint']);
+  	grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
