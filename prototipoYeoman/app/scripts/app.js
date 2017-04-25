5224b50365b87d39e3fb701e9d5b407e147ccdeb
Angelica Guaman <angelica.guaman.albarracin@gmail.com>
2017-04-25 22:31:02 +0200

    added image

--- prototipoYeoman/app/scripts/app.js
+++ prototipoYeoman/app/scripts/app.js
@@ -22,7 +22,7 @@ angular
   ])
   .config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/', { //index
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
--- prototipoYeoman/app/scripts/controllers/main.js
+++ prototipoYeoman/app/scripts/controllers/main.js
@@ -19,21 +19,41 @@ angular.module('prototipoYeomanApp')
 	$scope.slides = [
 		{
 			title:'Aprende a mantenerte en forma',
-			image:baseURL+'sports/',
+			img:baseURL+'sports/',
 			text:'¡Practica algún deporte todos los días!',
 			id:currentIndex++
 		},
 		{
 			title:'Buena alimentación',
-			image:baseURL+'food/',
+			img:baseURL+'food/',
 			text:'¡La importancia de frutas y verduras en la dieta!',
 			id:currentIndex++
 		},
 		{
 			title:'En contacto con la naturaleza',
-			image:baseURL+'nature/',
+			img:baseURL+'nature/',
 			text:'¡Mantente en armonía con la naturaleza!',
 			id:currentIndex++
+		}
+	];
+
+	baseURL = 'http://lorempixel.com/200/200/'; //tamaño de imágenes 960x450
+
+	$scope.contenido = [
+		{
+			img:baseURL+'people',
+			title:'Sobre nosotros',
+			summary:'Somos una empresa comprometida con la vida sana'
+		},
+		{
+			img:baseURL+'business',
+			title:'Nuestros servicios',
+			summary:'Ofrecemos asesoría profesional para mantenerse Sanos. buena alimentación'
+		},
+		{
+			img:baseURL+'transport',
+			title:'Contáctanos',
+			summary:'#333, Buena vida Online, Plaza Central, Duranque, Zip-432167'
 		}
 	];
 });
