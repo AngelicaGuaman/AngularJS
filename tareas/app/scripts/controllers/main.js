1b1fcaeb5e962b604cb25d046bff38961f3e4fbe
Angelica Guaman <angelica.guaman.albarracin@gmail.com>
2017-04-25 19:04:38 +0200

    corrected exercise about localStorage

--- tareas/app/scripts/controllers/main.js
+++ tareas/app/scripts/controllers/main.js
@@ -2,16 +2,18 @@
 
 var app = angular.module('tareasApp');
 
-app.controller('MainCtrl', ['$scope', 'localStorageService', function($scope, localStorageService) {
+app.controller('MainCtrl', function($scope, localStorageService) {
     
-    var tareasEnAlmacen = localStorageService.get('tareas');
-
-    $scope.tareas = tareasEnAlmacen && tareasEnAlmacen.split('\n') || [];
+    if (localStorageService.get('ls-tareas')){
+        $scope.tareas = localStorageService.get('ls-tareas');
+    }else{
+        $scope.tareas = [];
+    }
 
     //listener de angular
     //para vigilar los cambios que se producen en tareas (añaden o borran)
-    $scope.$watch('tareas', function (){
-        localStorageService.add('tareas', $scope.tareas.join('\n'));
+    $scope.$watchCollection('tareas', function (){
+        localStorageService.set('ls-tareas', $scope.tareas);
     });
 
     $scope.addTarea = function (){
@@ -19,10 +21,9 @@ app.controller('MainCtrl', ['$scope', 'localStorageService', function($scope, lo
     	$scope.tarea = '';
     };
 
-    $scope.eliminarTarea = function (indice){
+    $scope.eliminarTarea = function (element){
     	//si se utilizan estas líneas hay que pasarle el elemento
-    	//var pos = $scope.tareas.indexOf(element); 
-    	//$scope.tareas.splice(pos,1);
-    	$scope.tareas.splice(indice,1);
+    	var pos = $scope.tareas.indexOf(element); 
+    	$scope.tareas.splice(pos,1);
     };
-}]);
+});
