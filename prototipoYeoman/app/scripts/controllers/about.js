b02bac5e621cfeed427371581142a95d18ebc658
Angelica Guaman <angelica.guaman.albarracin@gmail.com>
2017-04-25 19:18:51 +0200

    Prototype use yeoman

---
+++ prototipoYeoman/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prototipoYeomanApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the prototipoYeomanApp
+ */
+angular.module('prototipoYeomanApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ prototipoYeoman/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prototipoYeomanApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the prototipoYeomanApp
+ */
+angular.module('prototipoYeomanApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
